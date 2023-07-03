a5ed6243eae321a3f41d936f69a8e013785ace4f
marcela rocha <marcelarochamartins@gmail.com>
2023-07-03 16:03:51 +0200

    job and cv models done

---
+++ models/Job.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const jobSchema = new Schema(
+  {
+    employerName: {
+      type: String,
+      required: true
+    },
+    employerLogo: {
+      type: String
+    },
+    jobApplicationLink: {
+      type: String,
+      required: false
+    },
+    jobApplicationDescription: {
+      type: String,
+      required: true
+    },
+    jobCountry: {
+      type: String,
+      required: true
+    },
+    jobCity: {
+      type: String,
+      required: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Job = model("Job", jobSchema);
+
+module.exports = Job;
