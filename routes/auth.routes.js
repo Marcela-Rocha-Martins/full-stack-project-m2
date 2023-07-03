7d567a9ed4d0d969006c62fc70ed6af4800bbe82
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-07-03 16:38:54 +0200

    Do bcrypt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,17 @@
 const { Router } = require("express");
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
 
 const router = new Router();
-router.post("/signup", (req, res) => {
-  res.send("Hello from the signup page");
+router.post("/signup", (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  bcryctjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      console.log(`Password hash: ${hashedPassword}`);
+    })
+    .catch((error) => next(error));
 });
 module.exports = router;
