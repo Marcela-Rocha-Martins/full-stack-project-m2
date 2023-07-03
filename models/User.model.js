b5063b3ed6fffd2837ac3c36d92efd31d7ca30a5
Joseph Segbefia <elorma.segbefia@gmail.com>
2023-07-03 17:51:09 +0200

    User Signup

--- models/User.model.js
+++ models/User.model.js
@@ -13,18 +13,19 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: [true, "Email is required!"],
+      match: [/^\S+@\S+\.\S+$/, "Provide a valid email address please!"],
       unique: true,
       lowercase: true,
       trim: true
+    },
+    avatarUrl: {
+      type: String
+    },
+    appliedJobs: [{ type: Schema.ObjectId, ref: "Job" }],
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required!"]
     }
-    // avatarUrl: {
-    //   type: String
-    // },
-    // appliedJobs: [{ type: Schema.ObjectId, ref: "Job" }],
-    // passwordHash: {
-    //   type: String,
-    //   required: [true, "Password is required!"]
-    // }
   },
   {
     timestamps: true
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,9 +5,24 @@ const saltRounds = 10;
 
 const router = new Router();
 
+router.get("/signup", (res, req) => {});
+
 router.post("/signup", (req, res, next) => {
   const { username, email, password } = req.body;
 
+  if (!username || !email || !password) {
+    res.render("auth/signup", { errorMessage: "All fields are mandatory" });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
+    });
+    return;
+  }
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -18,9 +33,26 @@ router.post("/signup", (req, res, next) => {
         passwordHash: hashedPassword
       });
     })
-    .then((user) => {
-      console.log("Newly created user is: ", user);
-    })
-    .catch((error) => next(error));
+    // Uncomment this line later
+    // .then(res.redirect('/userProfile'))
+    .catch((error) => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        console.log(
+          "Username and email need to be unique. Either username or email is already taken"
+        );
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "User not found and/or incorrect password and email combination."
+        });
+      } else {
+        next(error);
+      }
+    });
 });
+
+router.get("/login", (req, res) => res.send("Hello from the login page"));
+
+router.get("/userProfile", (req, res) => res.send("This is your profile Page"));
 module.exports = router;
